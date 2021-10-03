c254bffddbb0e470730f2a9273cf6386427aa662
KWilliams92 <kiara.williams@bryanuniversity.com>
2021-10-03 14:22:29 -0500

    Submit events w JS

--- week5/submit_events/submit_events.js
+++ week5/submit_events/submit_events.js
@@ -6,12 +6,15 @@ btn.addEventListener("click", info);
 function fName(){
     var a = document.getElementById("fName").value;
     alert(a);
+    document.getElementById('fName').value = "";
 }
  function lName(){
     var b = document.getElementById("lName").value;
     alert(b);
+    document.getElementById('lName').value = "";
 }
 function info(){
     var c = document.getElementById("info").value;
+    document.getElementById('info').value = "";
     alert(c);
 }
