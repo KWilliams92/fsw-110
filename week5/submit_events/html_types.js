dcc3d0121d535d8653e94bfa8a89303c201bf975
KWilliams92 <kiara.williams@bryanuniversity.com>
2021-10-15 00:19:22 -0500

    submit events and html types

--- week5/submit_events/html_types.js
+++ week5/submit_events/html_types.js
@@ -1,24 +1,2 @@
 var btn = document.getElementById("myForm");
 btn.addEventListener("click", Alert);
-btn.addEventListener("click", Alert);
-btn.addEventListener("click", Alert);
-btn.addEventListener("click", Alert);
-btn.addEventListener("click", Alert);
-btn.addEventListener("click", Alert);
-
-
-function Alert(){
-    var a = document.getElementById("fName").value;
-
-    var b = document.getElementById("lName").value;
-
-    var c = document.getElementById("info").value;
-
-    var d = document.getElementsByName("q1").value;
-
-    var e = document.getElementsByName("dropdown").value;
-
-    var f = document.getElementById("q2").value;
-
-    alert(`${a}`,`\n${b}`,`\n${c}`,`\n${d}`,`\n${e}`, `\n${e}`, `\n${f}`);
-}
