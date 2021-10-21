9ecc1380ea7f9fa549aeb464b79dd494763e89ce
KWilliams92 <kiara.williams@bryanuniversity.com>
2021-10-21 00:20:50 -0500

    changes made to other html types

--- week5/submit_events/html_types.js
+++ week5/submit_events/html_types.js
@@ -3,9 +3,9 @@ const form = document.getElementById('myForm');
 const fName = document.getElementById('fName');
 const lName = document.getElementById('lName');
 const age = document.getElementById('age');
-
 const options = document.getElementById('menu');
-const server = document.getElementById('server');
+
+const server = document.getElementById('server').value;
 
 btn = document.getElementById('button');
     btn.addEventListener('click', menuOpts);
@@ -23,6 +23,7 @@ btn = document.getElementById('button');
         }
 
 
-alert('First name: ' + fName.value + "\nLast name: " + lName.value + "\nWhat did you order today? " + food + "\nWould you order it again? " + options.value +"\nWho was your server? " + server.value);
-document.getElementById('form').reset();
+
+alert('First name: ' + fName.value + "\nLast name: " + lName.value + "\nWhat did you order today? " + food + "\nWould you order it again? " + options.value +"\nWho was your server? " + server);
+document.getElementById('myform').reset();
     }
